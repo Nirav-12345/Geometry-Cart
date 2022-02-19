014cd992705f0d28e36220c11453701cd574a72a
Nirav-12345 <Nirav-12345>
2022-02-18 22:25:59 -0800

    adding greetings file

---
+++ CartasianLength.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartasianLength;
+
+    public class CartasianLength
+    {
+        public static void CarLen()
+    {
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            if (x1 < 0 || y1<0 || x2 <0 || y2<0)
+
+            {
+                Console.WriteLine("Please enter positive number");
+            }
+
+            else if (x1 == 0 || y1 == 0 || x2 == 0 || y2 == 0)
+            {
+                Console.WriteLine("Please enter greater than zero");
+            }
+
+            else
+            {
+
+            double g = Math.Pow((x2 - x1),2);
+            double h = Math.Pow((y2 - y1),2);
+            double length = g+h;
+
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            
+
+
+
+            Console.WriteLine("The Length Of Cart" + length+ "in cm");
+            
+        }
+        
+
+    }
+        
+
+    }
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CartasianGreet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to LineComparison ComputationProgram on");
+    }
+}
+    
